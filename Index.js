6d10f6363b6fd8834709916106c95433fc947f45
Dinel1337 <62209705+Dinel1337@users.noreply.github.com>
2025-04-24 18:47:22 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -241,7 +241,7 @@ TelegramWebApp.MainButton.onClick(async () => {
         if (get_phone() === null) {
             phone_to_use = await request_user_phone();
         } else {
-            phone_to_use = get_phone();
+            phone_to_use = await request_user_phone();
             print(phone_to_use, 'ТЕЛЕФОНЧЕПК')
             step2();
         }
